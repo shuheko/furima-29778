a12b660a8a196da35787f8268751bd62614ccef4
shuheko <shuheko181@gmail.com>
2020-10-05 17:46:25 +0900

    商品出品機能３

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    #name           {"hoge"}
-    #explanation    {"アイウエオ"}
-    #category_id    {3}
-    #starus_id      {2}
-    #shipping_id    {2}
-    #area_id        {17}
-    #days_id        {2}
-    #price          {"10000"}
-    #user_id        {2}
+    name           {"hoge"}
+    explanation    {"アイウエオ"}
+    category_id    {3}
+    starus_id      {2}
+    shipping_id    {2}
+    area_id        {17}
+    days_id        {2}
+    price          {"10000"}
+    user_id        {2}
   end
 end
