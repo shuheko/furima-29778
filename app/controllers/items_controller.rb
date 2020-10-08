130ca2664ae483e2c166bd595358abfb525969ae
shuheko <shuheko181@gmail.com>
2020-10-08 19:31:45 +0900

    編集機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
-  before_action :set_item, only: [:show, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
+  before_action :edit_block, only: :edit
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -29,6 +30,17 @@ class ItemsController < ApplicationController
       end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
@@ -37,5 +49,12 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def edit_block
+    if user_signed_in? && current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
 
 end
