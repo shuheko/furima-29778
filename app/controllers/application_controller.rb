398c67eda9408f55e95cea17e4d77cfe6fadd7e3
shuheko <shuheko181@gmail.com>
2020-10-16 10:25:01 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :basic_auth
     #before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
